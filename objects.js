0593f56f3e25fd34bc59e8a01cbdaba804e7e529
Isak La Fleur <isak.engdahl@se.atlascopco.com>
2017-02-21 17:00:20 +0100

    .

--- objects.js
+++ objects.js
@@ -3,6 +3,7 @@
 // ======================
 
 /* eslint-disable no-alert, no-console */
+
 /*
 var name = 'Johan';
 
@@ -81,7 +82,7 @@ var anotherPerson4 = Object.create(person3);
 
 console.log(anotherPerson4.age);
 
-*/
+
 
 var person = {
     name: 'Isak',
@@ -94,8 +95,186 @@ Object.prototype.greet = function() {
 
 var isak = Object.create(person);
 var anna = Object.create(person);
-anna.name = 'Anna'; // if I remove the name property for anna then it will go up the property chain and find the name of person => 'Isak'
+//anna.name = 'Anna'; // if I remove the name property for anna then it will go up the property chain and find the name of person => 'Isak'
 
 // Goes up in the Protoype chain
-isak.greet();
-anna.greet();
+//isak.greet();
+//anna.greet();
+
+console.log(anna.__proto__ == person); //true
+console.log(anna.__proto__.__proto__ == Object.prototype); //true
+console.log(Object.getPrototypeOf(anna) == person); //true
+
+/////////////////////////////////////////////////
+// CONSTRUCTOR FUNCTIONS
+////////////////////////////////////////////////
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/constructor
+
+function Person() {
+    this.name = 'Isak';
+    this.greet = function() {
+        console.log('Hello, I am ' + this.name);
+    }
+
+}
+
+Person.prototype.greetGeneral = function() {
+    console.log('Hello');
+};
+
+// Contructor Function
+var person = new Person();
+person.name = 'Pelle';
+//console.log(person.name);
+person.greet();
+
+var anotherPerson = new Person();
+anotherPerson.greetGeneral();
+
+console.log(person instanceof Person);
+
+
+
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+var isak = new Person('Isak', 34);
+console.log(isak);
+
+
+
+////////////////////////////////////////////
+// SUMMARY 
+////////////////////////////////////////////
+
+//First option to create an object
+var person1 = {
+    name: 'Isak',
+    age: 34
+};
+
+console.log(person1 instanceof Object);
+
+//Second option to create an object
+var person2 = new Object();
+person2.name = 'Isak';
+person2.age = 34;
+
+console.log(person2 instanceof Object);
+
+//Third option to create an object
+var person3 = Object.create(null);
+person3.name = 'Isak';
+person3.age = 34;
+
+console.log(person3 instanceof Object);
+
+//Forth option to create an object
+function Person() {
+    this.name = 'Isak';
+    this.age = 34;
+}
+
+var person4 = new Person();
+
+console.log(person4 instanceof Person);
+
+
+/////////////////////////////////////////////
+// THIS keyword
+////////////////////////////////////////////
+
+// http://stackoverflow.com/questions/15455009/javascript-call-apply-vs-bind/31922712#31922712
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Operators/this
+
+
+function fn(message) {
+    console.log(message + ' ' + this); // this referce to the window object
+}
+
+var obj = {
+    obfn: fn // now "this"" refere to the object
+};
+
+var person = {
+    name: 'Isak'
+};
+
+obj.obfn();
+obj.obfn.bind(this)(); // now referce to the window object again, by using the bind method.
+obj.obfn.bind(person.name, 'Hello')(); // now referce to the person object.
+obj.obfn.call(person.name, 'Hello'); // is called instantly
+obj.obfn.apply(person.name, ['Hello']); // in an apply it is called instantly, but uses an array
+
+*/
+/*
+var account = {
+    cash: 12000,
+    _name: 'Default',
+    withdraw: function(amount) {
+        this.cash -= amount;
+        console.log('Withdrew ' + amount + ', new cash reserve is: ' + this.cash);
+    }
+};
+*/
+/*
+Object.defineProperty(account, 'deposit', {
+    value: function(amount) {
+        this.cash += amount;
+    }
+});
+
+account.deposit(3000);
+account.withdraw(1000);
+*/
+
+// from default, read-only. Need to add writable to true
+/*
+Object.defineProperty(account, 'name', {
+    value: 'ID000-1',
+    writable: true
+});
+
+console.log(account.name);
+account.name = 'ID000-3';
+console.log(account.name);
+*/
+/*
+Object.defineProperty(account, 'name', {
+    get: function() {
+        return this._name;
+    },
+    set: function(name) {
+        if (name === 'Isak') {
+            this._name = name;
+        }
+    }
+});
+
+account.name = 'ID000-3';
+console.log(account.name);
+*/
+
+var person = {
+    name: 'Isak',
+    age: 34,
+    greet: function() {
+        console.log('Hello');
+    }
+};
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects
+
+// delete a property from an object
+//delete person.name;
+
+// check if property exist in person object.
+console.log('name' in person);
+
+// loop through an object using the for ... of loop
+for (var field in person) {
+    console.log(person[field]);
+}
